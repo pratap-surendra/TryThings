9c2087f1ba35e1e7203f9c77f6b4c1bc1ae773bf
suren <suren@DESKTOP-GLO8DAC>
2023-04-04 22:43:12 +0530

    Add project files.

---
+++ TryBenchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BenchmarkDotNet.Running;
+using TryBenchmark;
+
+Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<TestBenchmark>();
+
---
+++ TryBenchmark/TestBenchmark.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+
+namespace TryBenchmark
+{  /*Run in Release Mode to see Benchmark in Action*/
+    public class TestBenchmark
+    {
+        [Params(100, 1000, 10000)]
+        public int N;
+      
+        [Benchmark]
+        public void Concatnate()
+        {
+            string resString=string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                resString += i;
+            }
+        }
+
+        [Benchmark]
+        public void StringBuilder()
+        {
+            StringBuilder resString=new ("");
+            for (int i = 0; i < N; i++)
+            {
+                resString.Append(i);
+            }
+            
+        }
+    }
+}
